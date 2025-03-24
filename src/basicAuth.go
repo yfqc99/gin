f420f303d4e3e7cb5d57fd5cb1b207eef4e565d9
yfqc99 <1927836028@qq.com>
2025-03-24 20:56:27 +0800

    gin框架基础

--- src/basicAuth.go
+++ src/basicAuth.go
@@ -20,6 +20,7 @@ func main() {
 	// 路由组使用 gin.BasicAuth() 中间件
 	// gin.Accounts 是 map[string]string 的一种快捷方式
 	authorized := r.Group("/admin", gin.BasicAuth(gin.Accounts{
+		// 定义用户名和密码的映射
 		"foo":    "bar",
 		"austin": "1234",
 		"lena":   "hello2",
@@ -28,10 +29,13 @@ func main() {
 
 	// /admin/secrets 端点
 	// 触发 "localhost:8080/admin/secrets
+	// 需要在请求头中添加Authorization，值为Basic base64(username:password)
 	authorized.GET("/secrets", func(c *gin.Context) {
 		// 获取用户，它是由 BasicAuth 中间件设置的
+		//获取通过 BasicAuth 中间件认证的用户名
 		user := c.MustGet(gin.AuthUserKey).(string)
 		if secret, ok := secrets[user]; ok {
+			//根据用户名从 secrets 映射中检索私人数据，并返回 JSON 格式的响应
 			c.JSON(http.StatusOK, gin.H{"user": user, "secret": secret})
 		} else {
 			c.JSON(http.StatusOK, gin.H{"user": user, "secret": "NO SECRET :("})
